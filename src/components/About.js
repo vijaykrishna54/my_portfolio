1ce68ae3541da79cf4efb0f85f4a95e80d3f73df
vijaykrishna54 <yarabarlavijaykrishna1437@gmail.com>
2023-09-04 17:16:10 -0700

    add about and project files along with their styles files

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import './stylesforcomponents/About.css';
 
 const About = () => {
-  return <div>I am a Web Developer!</div>;
+  return (
+    <div className="about-container">
+      <h2 className="about-heading">ABOUT ME</h2>
+      <div className='small-intro'>
+        <p>
+            I am a MERN stack web developer with a passion for creating web applications that deliver a great user experience.
+          </p>
+        </div>
+      <div className="content-container">
+        
+       
+        <div className="details">
+          <p>
+            As a web developer with expertise in the MERN stack, I am passionate about building robust and scalable web applications that deliver exceptional user experiences.
+          </p>
+          <p>
+            I have a solid foundation in JavaScript, Node.js, React, and MongoDB, as well as HTML5 and CSS. I have successfully completed multiple projects across the full software development lifecycle, from conceptualization to deployment, in my previous role at TCS, a leading IT services company.
+          </p>
+          <p>
+            I have recently completed a post-degree diploma in web and mobile application design and development at Langara College, where I enhanced my skills and knowledge of the latest industry trends and technologies. I am driven by a desire to learn new things and solve complex challenges, and I am always eager to adopt best practices and deliver cutting-edge solutions. I am looking for opportunities to join a dynamic and innovative team, where I can contribute to the success of web and mobile applications that exceed client expectations.
+          </p>
+          <p>
+            Feel free to connect with me on{' '}
+            <a
+              href="https://www.linkedin.com/in/vijay-krishna-yarabarla"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            .
+          </p>
+        </div>
+        <div className="skills">
+          <h3>My Skills:</h3>
+          <div className="skill-buttons">
+            <button className="skill-button">HTML 5</button>
+            <button className="skill-button">CSS 3</button>
+            <button className="skill-button">JavaScript</button>
+            <button className="skill-button">MongoDB</button>
+            <button className="skill-button">Express.js</button>
+            <button className="skill-button">React</button>
+            <button className="skill-button">Node.js</button>
+            <button className="skill-button">WordPress</button>
+            <button className="skill-button">Git</button>
+            <button className="skill-button">GitHub</button>
+            <button className="skill-button">Postman</button>
+            <button className="skill-button">Figma</button>
+            <button className="skill-button">AWS Cloud</button>
+            <button className="skill-button">Python</button>
+            <button className="skill-button">GraphQL</button>
+            <button className="skill-button">SQL</button>
+            <button className="skill-button">React Native</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './stylesforcomponents/Projects.css'; // Import your CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Projects = () => {
-  return <div>These are the projects I have developer so far!!</div>;
+  const [currentProject, setCurrentProject] = useState(0);
+
+  // Define an array of project objects with image, title, and description
+  const projects = [
+    {
+      title: 'Weather App',
+      image: require('./component_images/Weather_app.jpeg'),
+      description:
+        'Weather apps serve a wide range of purposes, from helping individuals plan their daily activities to providing critical information during severe weather events. They are valuable tools for staying informed and prepared for various weather conditions.',
+    },
+    {
+      title: 'Login Page',
+      image: require('./component_images/login_page.jpeg'),
+      description: 'Description of Project 2.',
+    },
+    {
+      title: 'Email Notifications',
+      image: require('./component_images/email_notifications.jpeg'),
+      description: 'Description of Project 3.',
+    },
+    {
+      title: 'Google Oauth',
+      image: require('./component_images/google_oauth.jpeg'),
+      description: 'Description of Project 4.',
+    },
+    {
+      title: 'Wordpress',
+      image: require('./component_images/word_press.jpeg'),
+      description: 'Description of Project 5.',
+    },
+  ];
+
+  const navigateToPreviousProject = () => {
+    setCurrentProject((prevProject) =>
+      prevProject === 0 ? projects.length - 1 : prevProject - 1
+    );
+  };
+
+  const navigateToNextProject = () => {
+    setCurrentProject((prevProject) =>
+      prevProject === projects.length - 1 ? 0 : prevProject + 1
+    );
+  };
+
+  return (
+    <div className="projects-container">
+      <h2>PROJECTS</h2>
+      <p>Here are some of the projects I've worked on:</p>
+
+      {/* Map over the projects array to display each project */}
+      <div className="project">
+        {/* Left column with project image */}
+        <div className="project-image">
+          <img
+            src={projects[currentProject].image.default}
+            alt={projects[currentProject].title}
+          />
+        </div>
+
+        {/* Right column with project details */}
+        <div className="project-details">
+          <h3>{projects[currentProject].title}</h3>
+          <p>{projects[currentProject].description}</p>
+          {currentProject !== 0 && (
+            <div className="arrow-left" onClick={navigateToPreviousProject}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </div>
+          )}
+          {currentProject !== projects.length - 1 && (
+            <div className="arrow-right" onClick={navigateToNextProject}>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+          )}
+          <button className="view-project-button">View Project</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
