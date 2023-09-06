3d460af1df3ccce1a083058bad0c78833714ad3c
vijaykrishna54 <yarabarlavijaykrishna1437@gmail.com>
2023-09-06 16:33:36 -0700

    Fix styles and alignment for About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './stylesforcomponents/About.css';
 
 const About = () => {
+  // Function to scroll to a specific section
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="about-container">
       <h2 className="about-heading">ABOUT ME</h2>
@@ -12,54 +20,69 @@ const About = () => {
       </div>
       <div className="content-container">
         <div className="details">
-          <p>
-            As a web developer with expertise in the MERN stack, I am passionate about building robust and scalable web applications that deliver exceptional user experiences.
-          </p>
-          <p>
-            I have a solid foundation in JavaScript, Node.js, React, and MongoDB, as well as HTML5 and CSS. I have successfully completed multiple projects across the full software development lifecycle, from conceptualization to deployment, in my previous role at TCS, a leading IT services company.
-          </p>
-          <p>
-            I have recently completed a post-degree diploma in web and mobile application design and development at Langara College, where I enhanced my skills and knowledge of the latest industry trends and technologies. I am driven by a desire to learn new things and solve complex challenges, and I am always eager to adopt best practices and deliver cutting-edge solutions. I am looking for opportunities to join a dynamic and innovative team, where I can contribute to the success of web and mobile applications that exceed client expectations.
-          </p>
-          <p>
-            Feel free to connect with me on{' '}
-            <a
-              href="https://www.linkedin.com/in/vijay-krishna-yarabarla"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-            .
-          </p>
+        <h3>Get to Know Me!</h3>
+<p>
+  I have a solid foundation in web development technologies, including JavaScript, Node.js, React, MongoDB, HTML5, and CSS. In my previous role at TCS, a well-respected IT services company, I successfully completed various projects that covered the entire software development process, from initial planning to final deployment.
+</p>
+<p>
+  I recently completed a post-degree diploma in web and mobile application design and development at Langara College. This educational experience allowed me to enhance my skills and stay up-to-date with the latest industry trends and technologies in web development.
+</p>
+<p>
+  I am actively seeking opportunities to join a dynamic and forward-thinking team. My goal is to contribute to the success of web and mobile applications by delivering solutions that not only meet but exceed client expectations.
+</p>
+<p>
+Please don't hesitate to connect with me on {' '}
+  <a
+    href="https://www.linkedin.com/in/vijay-krishna-yarabarla"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    LinkedIn 
+  </a>
+  .
+</p>
+
+
+
+
+
+
+
+
+
+
+          <button className="scroll-button" onClick={() => scrollToSection('contact')}>
+            Contact
+          </button>
         </div>
         <div className="side-content">
           <div className="skills">
             <h3>My Skills</h3>
             <div className="skill-buttons">
-            <button className="skill-button">HTML 5</button>
-            <button className="skill-button">CSS 3</button>
-            <button className="skill-button">JavaScript</button>
-            <button className="skill-button">React</button>
-            <button className="skill-button">React Native</button>
-            <button className="skill-button">Express</button>
-            <button className="skill-button">Node</button>
-            <button className="skill-button">MongoDB</button>
-            <button className="skill-button">SQL basics</button>
-            <button className="skill-button">GraphQL</button>
+              <button className="skill-button">HTML 5</button>
+              <button className="skill-button">CSS 3</button>
+              <button className="skill-button">JavaScript</button>
+              <button className="skill-button">React</button>
+              <button className="skill-button">React Native</button>
+              <button className="skill-button">Express</button>
+              <button className="skill-button">Node</button>
+              <button className="skill-button">MongoDB</button>
+              <button className="skill-button">SQL basics</button>
+              <button className="skill-button">GraphQL</button>
             </div>
           </div>
           <div className="tools">
             <h3>Tools</h3>
             <div className="tool-buttons">
-            <button className="tool-button">Git</button>
-            <button className="tool-button">GitHub</button>
-            <button className="tool-button">Jira</button>
-            <button className="tool-button">Trello</button>
-            <button className="tool-button">Wordpress</button>
-            <button className="tool-button">Postman</button>
+              <button className="tool-button">Git</button>
+              <button className="tool-button">GitHub</button>
+              <button className="tool-button">Jira</button>
+              <button className="tool-button">Trello</button>
+              <button className="tool-button">Wordpress</button>
+              <button className="tool-button">Postman</button>
             </div>
           </div>
+          
         </div>
       </div>
     </div>
