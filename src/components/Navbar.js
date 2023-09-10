74e948fa30cfef1d6e2259a3f5073bc1cd38720f
vijaykrishna54 <yarabarlavijaykrishna1437@gmail.com>
2023-09-09 19:34:26 -0700

    Added hamburger to menu for mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './stylesforcomponents/Navbar.css';
 import profilePicture from './component_images/Vijay_profile_picture.jpeg'; // Import your profile picture
 
@@ -10,28 +10,79 @@ const Navbar = () => {
     }
   };
 
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Detect screen width to determine when to show/hide the mobile menu
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setMobileMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Check initial screen width
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="left-content">
         <div className="profile-picture-container">
           <img src={profilePicture} alt="Profile" className="profile-picture" />
         </div>
-        <span className="name" onClick={() => scrollToSection('home')}>VIJAY KRISHNA</span>
+        <span className="name" onClick={() => scrollToSection('home')}>
+          VIJAY KRISHNA
+        </span>
       </div>
-      <ul>
-        <li>
-          <button onClick={() => scrollToSection('home')}>HOME</button>
-        </li>
-        <li>
-          <button onClick={() => scrollToSection('about')}>ABOUT</button>
-        </li>
-        <li>
-          <button onClick={() => scrollToSection('projects')}>PROJECTS</button>
-        </li>
-        <li>
-          <button onClick={() => scrollToSection('contact')}>CONTACT</button>
-        </li>
-      </ul>
+      {window.innerWidth <= 768 ? (
+        // Show hamburger menu on mobile screens
+        <button className="hamburger-button" onClick={toggleMobileMenu}>
+          ☰
+        </button>
+      ) : (
+        // Show all options on laptop screens
+        <ul>
+          <li>
+            <button onClick={() => scrollToSection('home')}>HOME</button>
+          </li>
+          <li>
+            <button onClick={() => scrollToSection('about')}>ABOUT</button>
+          </li>
+          <li>
+            <button onClick={() => scrollToSection('projects')}>PROJECTS</button>
+          </li>
+          <li>
+            <button onClick={() => scrollToSection('contact')}>CONTACT</button>
+          </li>
+        </ul>
+      )}
+
+      {/* Mobile menu */}
+      {isMobileMenuOpen && (
+        <div className="mobile-menu">
+          <ul>
+            <li>
+              <button onClick={() => scrollToSection('home')}>HOME</button>
+            </li>
+            <li>
+              <button onClick={() => scrollToSection('about')}>ABOUT</button>
+            </li>
+            <li>
+              <button onClick={() => scrollToSection('projects')}>PROJECTS</button>
+            </li>
+            <li>
+              <button onClick={() => scrollToSection('contact')}>CONTACT</button>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
